5130565b7011c456d03730d5d2fcc3aee6d1590d
Pallavi Baljekar <pallavibaljekar@gmail.com>
2017-04-21 15:11:36 -0400

    Added README

--- af_feats/make_AF_bin_feats.sh
+++ af_feats/make_AF_bin_feats.sh
@@ -9,7 +9,7 @@ fi
 PHONELIST=festival/clunits/phonenames
 SOURCE_DIR=af_feats/
 TARGET_DIR=binary_af_feats/
-rm -rf ${TARGET_DIR}
+#rm -rf ${TARGET_DIR}
     cat etc/txt.done.data|
     awk '{print $2}'|
     while read file
